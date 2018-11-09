ea39fcf3752a1f745a0f96dc1562222feebc0d2d
Taaseen Ali <taaseen.ali89@gmail.com>
2018-11-09 01:10:45 -0500

    done

--- main.c
+++ main.c
@@ -1,10 +1,34 @@
-#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+
+static void sighandler(int signo){
+  if(signo == SIGINT){
+    int file = open("errlog.txt", O_WRONLY | O_CREAT , 0666);
+    if(file<0){
+      printf("Error opening file: %s\n", strerror(errno));
+      exit(0);
+    }
+    write(file, "Exit due to SIGINT\n", 19);
+    close(file);
+    exit(0);
+  }
+
+  if(signo == SIGUSR1){
+    printf("Parent process: %d\n", getppid());
+  }
+}
 
 int main(){
+  signal(SIGINT, sighandler);
+  signal(SIGUSR1, sighandler);
+
   while(1){
-    printf("Hi I'm: %d", getpid());
+    printf("Hi I'm %d\n", getpid());
     sleep(1);
   }
 }
